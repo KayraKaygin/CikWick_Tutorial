5d5a6b2c0ccbb4364d8297a20baaeca6185cd150
KayraKaygin <kayrakaygin524@gmail.com>
2025-08-26 23:28:10 +0300

    Camera Controller System

---
+++ Assets/_GameAssets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+
+{
+
+    [Header("References")]
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Transform _orientationTransform;
+    [SerializeField] private Transform _playerVisualTransform;
+
+    [Header("Settings")]
+
+    [SerializeField] private float _rotationSpeed;
+
+
+
+    void Update()
+    {
+        Vector3 viewDirection = _playerTransform.position - new Vector3(transform.position.x, _playerTransform.position.y, transform.position.z);
+
+        _orientationTransform.forward = viewDirection.normalized;
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 inputDirection = _orientationTransform.forward * verticalInput + _orientationTransform.right * horizontalInput;
+
+
+        if (inputDirection != Vector3.zero)
+        {
+            // Slerp = rotasyon işlemlerini daha yumuşak yapar
+            // Lerp = pozisyonlar için
+            _playerVisualTransform.forward = Vector3.Slerp(_playerVisualTransform.forward, inputDirection.normalized, Time.deltaTime * _rotationSpeed);
+        
+        }
+      
+        
+    }
+}
